af608c3b927ba144243a08aa41d72d3d5062c293
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-05-27 12:53:15 +0530

    Update basics.py

--- basics.py
+++ basics.py
@@ -31,3 +31,11 @@ for i in range(o,0,-1):
         print(i, end=" ");
     print("");
 print("DONE")
+
+rows = int(input("Enter the row size for the pattern: "))
+for i in range(1, rows + 1):  # Outer loop for rows
+    for j in range(rows  - i):  # Inner loop for spaces
+        print(" ", end=" ")
+    for k in range(1, 2 * i):  # Inner loop for stars
+        print("*", end=" ")
+    print()
