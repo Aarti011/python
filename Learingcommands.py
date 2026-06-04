a582e729766cef0b00a08f4c0c2c246d8cfc70b1
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-06-04 23:23:12 +0530

    Update Learingcommands.py

--- Learingcommands.py
+++ Learingcommands.py
@@ -1,3 +1,4 @@
+# class properties
 class Person:
   def __init__(self, name, age):
     self.name = name
@@ -7,3 +8,17 @@ p1 = Person("Emil", 36)
 
 print(p1.name)
 print(p1.age)
+
+
+
+class Car:
+  def __init__(self, brand, model):
+    self.brand = brand
+    self.model = model
+
+car1 = Car("Toyota", "Corolla")
+
+print(car1.brand)
+print(car1.model)
+
+
