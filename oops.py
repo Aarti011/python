24d131900ae2de80dfb0b51438766f87c589fc9e
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-06-08 22:38:27 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -9,3 +9,15 @@ class Animal:
 dog1 = Animal("Buddy", 3)
 print(dog1.name) 
 print(dog1.species)
+
+# Inheritance in OOPs
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("John", "Doe")
+x.printname()
