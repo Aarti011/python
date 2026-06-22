f2eaf15bb2f7071d2c12c8b83322160957c92350
AARTI KUMARI <157306665+Aarti011@users.noreply.github.com>
2026-06-22 19:48:43 +0530

    Update oops.py

--- oops.py
+++ oops.py
@@ -21,3 +21,32 @@ class Person:
 
 x = Person("John", "Doe")
 x.printname()
+
+
+
+
+# Amother oops concept
+class Person:
+    def __init__(self, name, roll, standard):
+        self.name=name
+        self.roll=roll
+        self.standard=standard
+
+stud1= Person("Anshika", 56, 7)
+print(stud1.name , stud1.roll, stud1.standard)
+
+stud2=Person("Manoj", 64, 7)
+print(stud2.name)
+print(stud2.roll)
+print(stud2.standard)
+
+stud3=Person("Kanishq", 23, 7)
+print(stud3.name)
+print(stud3.roll)
+print(stud3.standard)
+
+
+
+
+
+
